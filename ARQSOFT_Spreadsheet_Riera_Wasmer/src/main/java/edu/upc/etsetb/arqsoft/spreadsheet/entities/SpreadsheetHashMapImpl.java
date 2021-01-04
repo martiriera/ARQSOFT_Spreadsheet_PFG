f96bb7f08a585e6e5024c83dbdfac2e4445c18cc
reir <martiriera@github.com>
2021-01-04 19:44:13 +0100

    Range of cells first version

--- ARQSOFT_Spreadsheet_Riera_Wasmer/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/entities/SpreadsheetHashMapImpl.java
+++ ARQSOFT_Spreadsheet_Riera_Wasmer/src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/entities/SpreadsheetHashMapImpl.java
@@ -6,7 +6,9 @@
 package edu.upc.etsetb.arqsoft.spreadsheet.entities;
 
 import static java.lang.Math.abs;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -20,44 +22,45 @@ public class SpreadsheetHashMapImpl implements Spreadsheet {
         return cellMap.get(cellCoord);
     }
 
-    public Range getRangeofCells(CellCoordinateImpl originCellCoord, CellCoordinateImpl finalCellCoordinate) {
+    public Range getRangeofCells(CellCoordinateImpl originCellCoord, CellCoordinateImpl finalCellCoord) {
 
-        int numberOfColumns = 0;
-        String[] columnsToIterate = getColumnArray(numberOfColumns);
-        int numberOfRows = finalCellCoordinate.rowComponenent - originCellCoord.rowComponenent;
-        numberOfRows = Math.abs(numberOfRows);
+        List<String> columnsToIterate = this.getColumnArray(originCellCoord.columnComponent, finalCellCoord.columnComponent);
+        int numberOfColumns = columnsToIterate.size();
+        int numberOfRows = Math.abs(finalCellCoord.rowComponenent - originCellCoord.rowComponenent);
 
         HashMap<CellCoordinate, Cell> rangeMap = new HashMap<>();
 
-        for (int i = 0; i < columnsToIterate.length; i++) {
-            for (int j = 0; j < numberOfRows; j++) {
-             
-                
+        for (int columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            for (int rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+                CellCoordinateImpl cellToPutCoordinate = new CellCoordinateImpl(columnsToIterate.get(columnIndex), rowIndex);
+                Cell cellToPut = this.getCell(cellToPutCoordinate);
+                if (cellToPut != null) {
+                    rangeMap.put(finalCellCoord, cellToPut);
+                }
             }
         }
-    
+        return new RangeImpl(rangeMap);
 
-    return  new RangeImpl(rangeMap);
-
-}
-
-public static String[] getColumnArray(String initialColumn, String finalColumn) {
-
-        String[] columnArray = new String[0];
-        int i = 0;
-        int arrayLength = 0;
-        String aux = initialColumn;
-        while(aux != finalColumn) {
-            char c = (char) ('A' + (i % 26));
-            aux = c + "";
-            if (i > 25) {
-                aux = columnArray[(i / 26) - 1] + "" + c;
-            }
-            columnArray[i] = aux;
-            arrayLength++;
-            i++;
-        }
-        return columnArray;
     }
 
+    public List<String> getColumnArray(String initialColumn, String finalColumn) {
+        ArrayList<String> columnArray = new ArrayList();
+        int i = 0;
+        String aux = initialColumn;
+        while (!aux.equals(finalColumn)) {
+            char c = (char) ('A' + (i % 26));
+            aux = c + "";
+            System.out.println(aux);
+            if (i > 25) {
+                aux = columnArray.get((i / 26) - 1) + "" + c;
+            }
+            i++;
+            columnArray.add(aux);
+        }
+        int fromIndex = columnArray.indexOf(initialColumn);
+        int toIndex = columnArray.indexOf(finalColumn) + 1;
+        // Trim array from initial column to final column
+        return (columnArray.subList(fromIndex, toIndex));
+
+    }
 }
